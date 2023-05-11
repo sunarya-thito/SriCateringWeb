058439e990b8719aee28a94d0a9abca76084ef68
sunarya-thito <septogeddon@gmail.com>
2023-05-11 09:27:41 +0700

    Mulai pakai GoRouter

---
+++ lib/routes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:sricatering/admin/form/buat_paket_form.dart';
+import 'package:sricatering/admin_page.dart';
+
+GoRouter router = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    pageBuilder: (context, state) => MaterialPage(
+      child: Container(),
+    ),
+  ),
+  GoRoute(
+    path: '/admin',
+    pageBuilder: (context, state) => const MaterialPage(
+      child: AdminPage(),
+    ),
+    routes: [
+      GoRoute(
+        path: 'buat_paket',
+        pageBuilder: (context, state) => const MaterialPage(
+          child: BuatPaketForm(),
+        ),
+      ),
+    ],
+  ),
+]);
