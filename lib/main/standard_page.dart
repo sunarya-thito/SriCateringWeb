e1ad9e9480eef1ea74ce0602a07dc844889cd9a1
sunarya-thito <septogeddon@gmail.com>
2023-05-22 10:17:17 +0700

    smooth scrolling

--- lib/main/standard_page.dart
+++ lib/main/standard_page.dart
@@ -7,6 +7,7 @@ import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sricatering/main/page/home_page.dart';
 import 'package:sricatering/main/web_layout.dart';
+import 'package:web_smooth_scroll/web_smooth_scroll.dart';
 
 class StandardPage extends StatefulWidget {
   final Widget child;
@@ -63,156 +64,160 @@ class _StandardPageState extends State<StandardPage> {
           color: kHeaderColor,
           child: WebLayout(
             builder: (context, width) {
-              return CustomScrollView(
+              return WebSmoothScroll(
                 controller: _scrollController,
-                // physics: const NeverScrollableScrollPhysics(),
-                scrollBehavior: ScrollBehavior().copyWith(
-                  overscroll: false,
-                  scrollbars: true,
-                ),
-                slivers: [
-                  SliverAppBar(
-                    floating: true,
-                    pinned: true,
-                    expandedHeight: 200,
-                    flexibleSpace: Container(
-                      color: kHeaderColor,
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        width: width,
-                        child: Row(
-                          children: [
-                            MouseRegion(
-                              cursor: SystemMouseCursors.click,
-                              child: GestureDetector(
-                                onTap: () {
-                                  context.go('/');
-                                },
-                                child: Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(12.0),
-                                      child: Image.asset(
-                                        'assets/logo_white.png',
-                                        height: 80,
+                child: CustomScrollView(
+                  controller: _scrollController,
+                  physics: const NeverScrollableScrollPhysics(),
+                  scrollBehavior: ScrollBehavior().copyWith(
+                    overscroll: false,
+                    scrollbars: true,
+                  ),
+                  slivers: [
+                    SliverAppBar(
+                      floating: true,
+                      pinned: true,
+                      expandedHeight: 200,
+                      flexibleSpace: Container(
+                        color: kHeaderColor,
+                        alignment: Alignment.bottomCenter,
+                        child: Container(
+                          width: width,
+                          child: Row(
+                            children: [
+                              MouseRegion(
+                                cursor: SystemMouseCursors.click,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    context.go('/');
+                                  },
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(12.0),
+                                        child: Image.asset(
+                                          'assets/logo_white.png',
+                                          height: 80,
+                                        ),
                                       ),
+                                      Text(
+                                        'SRI CATERING',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 20),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Spacer(),
+                              profilePicture != null
+                                  ? TextButton(
+                                      onPressed: () {
+                                        context.go('/me');
+                                      },
+                                      style: TextButton.styleFrom(
+                                        primary: Colors.white,
+                                        padding: const EdgeInsets.only(
+                                          left: 16,
+                                          right: 0,
+                                          top: 8,
+                                          bottom: 8,
+                                        ),
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          Text(profileName ?? ''),
+                                          const SizedBox(width: 8),
+                                          CircleAvatar(
+                                            foregroundImage:
+                                                NetworkImage(profilePicture),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  : IconButton(
+                                      color: Colors.white,
+                                      onPressed: () {
+                                        FirebaseAuth.instance
+                                            .signInWithPopup(
+                                                GoogleAuthProvider()
+                                                    .setCustomParameters({
+                                          'prompt': 'select_account',
+                                        }))
+                                            .catchError(
+                                          (e) {
+                                            showDialog(
+                                              context: context,
+                                              builder: (context) {
+                                                return AlertDialog(
+                                                  title: Text('Error'),
+                                                  content: Text(e.message ??
+                                                      'Unknown error'),
+                                                  actions: [
+                                                    TextButton(
+                                                      onPressed: () {
+                                                        Navigator.pop(context);
+                                                      },
+                                                      child: Text('OK'),
+                                                    ),
+                                                  ],
+                                                );
+                                              },
+                                            );
+                                          },
+                                        );
+                                      },
+                                      icon: const Icon(Icons.login),
                                     ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    SliverToBoxAdapter(
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: kBackgroundColor,
+                        child: Container(
+                          width: width,
+                          child: widget.child,
+                        ),
+                      ),
+                    ),
+                    SliverToBoxAdapter(
+                      child: Container(
+                        color: kHeaderColor,
+                        alignment: Alignment.center,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 32),
+                          height: 200,
+                          width: width,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 300,
+                                child: Column(
+                                  children: [
                                     Text(
-                                      'SRI CATERING',
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 20),
-                                    ),
+                                        'Kampung Ciherang RT.02 RW.10 Banjaran, Kabupaten Bandung'),
+                                    const SizedBox(height: 12),
+                                    Row(
+                                      children: [
+                                        Icon(Icons.phone, color: Colors.white),
+                                        const SizedBox(width: 8),
+                                        Text('0812-2343-3254'),
+                                      ],
+                                    )
                                   ],
                                 ),
                               ),
-                            ),
-                            Spacer(),
-                            profilePicture != null
-                                ? TextButton(
-                                    onPressed: () {
-                                      context.go('/me');
-                                    },
-                                    style: TextButton.styleFrom(
-                                      primary: Colors.white,
-                                      padding: const EdgeInsets.only(
-                                        left: 16,
-                                        right: 0,
-                                        top: 8,
-                                        bottom: 8,
-                                      ),
-                                    ),
-                                    child: Row(
-                                      children: [
-                                        Text(profileName ?? ''),
-                                        const SizedBox(width: 8),
-                                        CircleAvatar(
-                                          foregroundImage:
-                                              NetworkImage(profilePicture),
-                                        ),
-                                      ],
-                                    ),
-                                  )
-                                : IconButton(
-                                    color: Colors.white,
-                                    onPressed: () {
-                                      FirebaseAuth.instance
-                                          .signInWithPopup(GoogleAuthProvider()
-                                              .setCustomParameters({
-                                        'prompt': 'select_account',
-                                      }))
-                                          .catchError(
-                                        (e) {
-                                          showDialog(
-                                            context: context,
-                                            builder: (context) {
-                                              return AlertDialog(
-                                                title: Text('Error'),
-                                                content: Text(e.message ??
-                                                    'Unknown error'),
-                                                actions: [
-                                                  TextButton(
-                                                    onPressed: () {
-                                                      Navigator.pop(context);
-                                                    },
-                                                    child: Text('OK'),
-                                                  ),
-                                                ],
-                                              );
-                                            },
-                                          );
-                                        },
-                                      );
-                                    },
-                                    icon: const Icon(Icons.login),
-                                  ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SliverToBoxAdapter(
-                    child: Container(
-                      alignment: Alignment.center,
-                      color: kBackgroundColor,
-                      child: Container(
-                        width: width,
-                        child: widget.child,
-                      ),
-                    ),
-                  ),
-                  SliverToBoxAdapter(
-                    child: Container(
-                      color: kHeaderColor,
-                      alignment: Alignment.center,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(vertical: 32),
-                        height: 200,
-                        width: width,
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 300,
-                              child: Column(
-                                children: [
-                                  Text(
-                                      'Kampung Ciherang RT.02 RW.10 Banjaran, Kabupaten Bandung'),
-                                  const SizedBox(height: 12),
-                                  Row(
-                                    children: [
-                                      Icon(Icons.phone, color: Colors.white),
-                                      const SizedBox(width: 8),
-                                      Text('0812-2343-3254'),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               );
             },
           ),
